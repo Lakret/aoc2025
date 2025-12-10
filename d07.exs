1ddbeec2d8ddd45c8ff924f939ea20d0134d5a9c
Dimitrii Slutskii <dmitrii.slutskii@atolls.com>
2025-12-10 11:35:33 +0100

    refactor

--- d07.exs
+++ d07.exs
@@ -39,16 +39,17 @@ defmodule Manifold do
 
       Map.merge(beams, next_level)
     else
-      current_beams = beams |> Enum.filter(fn {{row, _}, _} -> row == row_idx end)
-      Enum.reduce(current_beams, beams, fn {{row, col}, timelines}, beams ->
-        next_row = row + 1
+      beams
+      |> Enum.filter(fn {{row, _}, _} -> row == row_idx end)
+      |> Enum.reduce(beams, fn {{row, col}, timelines}, beams ->
+        row = row + 1
 
-        if {next_row, col} in splitters do
+        if {row, col} in splitters do
           beams
-          |> Map.update({next_row, col - 1}, timelines, &(&1 + timelines))
-          |> Map.update({next_row, col + 1}, timelines, &(&1 + timelines))
+          |> Map.update({row, col - 1}, timelines, &(&1 + timelines))
+          |> Map.update({row, col + 1}, timelines, &(&1 + timelines))
         else
-          Map.update(beams, {next_row, col}, timelines, &(&1 + timelines))
+          Map.update(beams, {row, col}, timelines, &(&1 + timelines))
         end
       end)
     end
@@ -69,16 +70,16 @@ defmodule Manifold do
       beams = split(splitters, row_idx, beams)
       {beams, n_splits + new_splits}
     end)
+    |> elem(1)
   end
 
     def p2(%__MODULE__{} = manifold) do
-
-            manifold.splitters
-            |> Enum.with_index()
-            |> Enum.reduce(%{manifold.start_pos => 1}, fn {splitters, row_idx}, beams->
-                split(splitters, row_idx, beams)
-            end)
-|> Enum.filter(fn {{row, _}, _} -> row == length(manifold.splitters) end)
+        manifold.splitters
+        |> Enum.with_index()
+        |> Enum.reduce(%{manifold.start_pos => 1}, fn {splitters, row_idx}, beams->
+            split(splitters, row_idx, beams)
+        end)
+        |> Enum.filter(fn {{row, _}, _} -> row == length(manifold.splitters) end)
         |> Enum.map(fn {_, timelines} -> timelines end)
         |> Enum.sum()
     end
@@ -110,10 +111,8 @@ test_manifold = Manifold.parse(test_input)
 input = File.read!("inputs/d07.txt")
 manifold = Manifold.parse(input)
 
-{_test_beams, test_n_splits} = Manifold.p1(test_manifold)
-assert test_n_splits == 21
-{_beams, n_splits} = Manifold.p1(manifold)
-assert n_splits |> IO.inspect(label: :p1) == 1543
+assert Manifold.p1(test_manifold) == 21
+assert Manifold.p1(manifold) |> IO.inspect(label: :p1) == 1543
 
 assert Manifold.p2(test_manifold) == 40
 assert Manifold.p2(manifold) |> IO.inspect(label: :p2) == 3223365367809
